1482ded8730c559a9c1a6c30d2f4d0eaf5a10b41
Renan Perin Ribeiro <renan.perinribeiro@outlook.com>
2023-02-11 22:09:59 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/template-form/template-form.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  usuario: any = {
+    nome: null,
+    email: null
+  }
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  onSubmit(form: any) {
+    this.http.post('https://httpbin.org/post', JSON.stringify(form.value)).pipe(map((res: any) => res))
+    .subscribe(dados => console.log(dados));
+  }
+
+  consultaCep(cep: any, form: any) {
+    cep.replace(/\D/g, '');
+
+    if (cep != "") {
+      var validacep = /^[0-9]{8}$/;
+
+      if(validacep.test(cep)) {
+
+        this.resetaDadosForm(form)
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json`).pipe(map((dados: any) => dados))
+        .subscribe(dados => this.populaDadosForm(dados, form));
+
+      }
+    }
+  }
+
+  populaDadosForm(dados: any, formulario: any) {
+    // formulario.setValue({
+    //   nome: formulario.value.nome,
+    //   email: formulario.value.email,
+    //   endereco: {
+    //     rua: dados.logradouro,
+    //     cep: dados.cep,
+    //     numero: '',
+    //     complemento: dados.complemento,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf
+    //   }
+    // })
+
+    formulario.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        // cep: dados.cep,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    })
+  }
+
+  resetaDadosForm(formulario: any) {
+    formulario.form.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    })
+  }
+
+}
